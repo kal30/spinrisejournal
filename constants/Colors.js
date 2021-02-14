30a6895fd314f8aa67727dfbcdf9c84a2e193982
Kalyani Rajamani <rkalyani@gmail.com>
2021-02-13 18:48:15 -0600

    Define some color constants

---
+++ constants/Colors.js
@@ -0,0 +1,17 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  primary: '#ffa500',
+  sendButtonColor: '#2e8b57',
+  disabledButtonColor: '#d3d3d3',
+};
